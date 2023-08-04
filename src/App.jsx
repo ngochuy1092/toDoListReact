a40493755834babec05119b2701d0593402bcd03
Nguyen Ngoc Huy <ngngochuy1092@gmail.com>
2023-08-04 20:29:59 +0700

    add list

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { useState  } from "react";
-export default function App() {
-  const [a, setA]= useState(1);
-  const change= ()=> {
-    setA(2)
-  }
-  console.log('hi')
+import { useState } from 'react';
+
+function App() {
+  
+  const [todos, setTodos] = useState([]);  
+  const [inputValue, setInputValue]= useState('');
+
+  const changeInputValue = (event) => {
+    setInputValue(event.target.value);
+   };
+
+  const displayList = () => {    
+    setTodos(function a(previousTodos) { return( [...previousTodos, inputValue])});
+    // Clear the input after adding a todo   
+    setInputValue('');
+   console.log(inputValue)
+  };
   return (
     <div>
-    <div>{a}</div>
-    <button onClick={change}>button</button>
+      <div>
+        <input onChange={changeInputValue} value={inputValue} type="text" />
+        <button onClick={displayList}>Add list</button>
+      </div>
+      <div>
+        {todos.map((todo, index) => (
+          <h1 key={index}>{todo}</h1>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
+
+export default App;
